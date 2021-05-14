59cf4c9dffd36c22fdc290331c4ae658a4fed6ea
rovira gajardo <francisca-05@hotmail.com>
2021-05-13 23:45:17 -0400

    ahhhhhhhhhhhhhhhhh

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+   $(".icon1").click(function(){
+   	
+    $(".parrafo1").toggle();
+
+
+   $(".icon2").click(function(){
+   	
+    $(".parrafo2").toggle(); 
+   
+
+   });
+
+})
+
+
+ 
+
+
+
+
+ $(".icon3").click(function(){
+   	
+    $(".parrafo3").toggle(); 
+
+
+
+   });
+
+})
